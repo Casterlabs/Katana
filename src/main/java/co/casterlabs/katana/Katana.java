15a06d09ee549a4ae81cbbc0994d40cb3a712962
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-02-05 17:26:49 -0600

    1.29.1

--- src/main/java/co/casterlabs/katana/Katana.java
+++ src/main/java/co/casterlabs/katana/Katana.java
@@ -31,7 +31,7 @@ import xyz.e3ndr.fastloggingframework.logging.FastLogger;
 
 @Getter
 public class Katana {
-    public static final String VERSION = "1.29.0";
+    public static final String VERSION = "1.29.1";
     public static final String SERVER_DECLARATION = String.format("Katana/%s (%s)", Katana.VERSION, System.getProperty("os.name", "Generic"));
 
     public static final File CONFIG_FILE = new File("config.json");
